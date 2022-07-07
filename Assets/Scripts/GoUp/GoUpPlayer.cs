c47048141d6ee353c42c28587df6306f997e1bb3
YijuKim <yongin0814@gmail.com>
2022-07-07 16:59:58 +0900

    1. player prefab 디자인 변경함 2. 로비디자인, 고업디자인 씬 만듦 (UI 꾸밈)

--- Assets/Scripts/GoUp/GoUpPlayer.cs
+++ Assets/Scripts/GoUp/GoUpPlayer.cs
@@ -13,7 +13,6 @@ public class GoUpPlayer : MonoBehaviour
 
     // 순위 별 올라가는 높이
     public float[] rankBonuses = new float[4];
-
     /// <summary>
     /// 등수만큼 올라가기
     /// </summary>
@@ -21,6 +20,7 @@ public class GoUpPlayer : MonoBehaviour
     {
         gameObject.transform.position += Vector3.up * rankBonuses[rank];
         correctNumber += 1;
+
     }
 
     public void GoDown()
